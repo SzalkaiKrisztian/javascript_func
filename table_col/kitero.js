ea15077d61f74cce38da0cf2c6b38ca15e79259a
krisztian <szalkai.krisztian_2022c@bolyai-szakkozep.hu>
2025-10-08 15:37:29 +0200

    kesz a scripthazi

--- table_col/kitero.js
+++ table_col/kitero.js
@@ -1,3 +1,26 @@
+/**@type {{neve:string,kor:string,szerelme1:string,szerelme2?:string}[]} */
+const arr =[
+    {//elso tr
+        neve: 'Balassi Bálint',
+        kor: 'reformáció',
+        szerelme1: 'Losonczy Anna',
+        szerelme2: 'Dobó Krisztina'
+    },{//masodik tr
+        neve: 'Csokonai Vitéz Mihály',
+        kor: 'felvilágosodás',
+        szerelme1: 'Vajda Juliána'
+    },{//harmadik tr
+        neve: 'Petőfi Sándor',
+        kor: 'magyar romantika',
+        szerelme1: 'Mednyánszky Berta',
+        szerelme2: 'Szendrey Júlia'
+    },{//4. tr
+        neve: 'Ady Endre',
+        kor: '20. század',
+        szerelme1: 'Léda',
+        szerelme2: 'Csinszka'
+   }
+] //tomb
 /** 
  *@type{string}
 */
@@ -25,4 +48,53 @@ const objektum={
 }
 for(const key in objektum){
     console.log(objektum[key])
+}
+//hf:
+const table =document.createElement('table')//tablat rakok
+document.body.appendChild(table)
+
+const thead = document.createElement('thead')//theadet rakok
+table.appendChild(thead)
+
+const tr =document.createElement('tr')
+thead.appendChild(tr)
+
+const th1 =document.createElement('th')//1. oszlop headerje
+th1.innerText="Szerző Neve"
+tr.appendChild(th1)
+
+const th2 = document.createElement('th')//2. -||-
+th2.innerText="Korszak"
+tr.appendChild(th2)
+
+const th3 = document.createElement('th') //3. -||-
+th3.innerText="Szerelmek"
+tr.appendChild(th3)
+th3.colSpan="2" // 2.tölt ki
+
+const tbody = document.createElement('tbody')
+table.appendChild(tbody)
+for(const hf of arr){
+    const tr1 = document.createElement('tr') //tr
+    tbody.appendChild(tr1)
+
+    const td1 = document.createElement('td')//szerzo neve
+    td1.innerText=hf.neve
+    tr1.appendChild(td1)
+
+    const td2 = document.createElement('td')//korszaka
+    td2.innerText=hf.kor
+    tr1.appendChild(td2)
+
+    const td3=document.createElement('td')//1. szerelme
+    td3.innerText=hf.szerelme1
+    tr1.appendChild(td3)
+
+    if(hf.szerelme2 === undefined){
+        td3.colSpan="2"
+    }else{
+        const td4=document.createElement('td')
+        td4.innerText=hf.szerelme2
+        tr1.appendChild(td4)
+    }
 }
--- table_col_hf/index.js
+++ table_col_hf/index.js
@@ -1,24 +1,24 @@
 /**@type {{theme: string, time:string, scientist1:string, scientist2?:string}[]} */
 const arr = [
-    {
-        theme: 'Optika',
+    {//tr1
+        theme: 'Optika', //td-k
         time: 'XI. szazad',
         scientist1: 'Alhazen'
     },
-    {
-        theme: 'Asztronómia',
+    {//tr2
+        theme: 'Asztronómia',//td-k
         time: 'reneszánsz',
         scientist1: 'Kepler',
         scientist2: 'Galilei'
     },
-    {
-        theme: 'Kvantumfizika',
+    {//tr3
+        theme: 'Kvantumfizika',//td-k
         time: 'XIX-XX. század',
         scientist1: 'Max Planck',
         scientist2: 'Albert Einstein'
     },
-    {
-        theme: 'Modern fizika',
+    {//tr4
+        theme: 'Modern fizika',//td-k
         time: 'XX-XXI. század',
         scientist1: 'Richard Feynman',
         scientist2: 'Stephen Hawking'
@@ -31,26 +31,46 @@ table.appendChild(thead)
 const tr = document.createElement('tr')
 thead.appendChild(tr)
 
-const th = document.createElement('th')
+const th = document.createElement('th') //1.th hozza adasa
 th.innerText = "Fizika területe"
 tr.appendChild(th)
 
-const th1 = document.createElement('th')
+const th1 = document.createElement('th') //2.th hozza adasa
 th1.innerText ="Időszak"
 tr.appendChild(th1)
 
-const th2 = document.createElement('th')
+const th2 = document.createElement('th') //3.th hozza adasa
 th2.innerText ="Képviselők"
 tr.appendChild(th2)
 
-th2.colSpan="2"
+th2.colSpan="2" //kitölt 2 helyet
 
 const tbody = document.createElement('tbody')
 table.appendChild(tbody)
-for( const seged of arr){
-    const tr1 = document.createElement('tr')
+for(const seged of arr){
+    const tr1 = document.createElement('tr') //a tr hozzarendelem
     tbody.appendChild(tr1)
-    const td = document.createElement('td')
-    td.innerText = arr.theme
-    tr.appendChild(td)
+    const td = document.createElement('td') //az  1.td meghatározása
+    td.innerText = seged.theme
+    tr1.appendChild(td)
+
+    const td1 = document.createElement('td') //az  2.td meghatározása
+    td1.innerText = seged.time
+    tr1.appendChild(td1)
+
+    const td2 = document.createElement('td')//az  3.td meghatározása
+    td2.innerText = seged.scientist1
+    tr1.appendChild(td2)
+
+    
+
+    if (seged.scientist2 === undefined){// van-e ott valami
+         td2.colSpan = 2 //ha, akkor colspan
+    }
+    else{
+    const td3 = document.createElement('td') //ugyan az mint felette csak elágazásban //az  4.td meghatározása
+    td3.innerText = seged.scientist2
+    tr1.appendChild(td3)
+    }
 }
+
