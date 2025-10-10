988a50f550a847154d9517c1c75a35bdd78f7ee9
Szalkai Krisztian Peter <szalkai.krisztian@BOLYAI.LOCAL>
2025-10-10 11:38:05 +0200

    bomba

---
+++ table_row/kitero.js
@@ -0,0 +1,13 @@
+/**@type {{name:string}[]} */
+const obj ={
+}
+const szam=3
+if(szam >5){
+    obj.name='harcsa'
+    
+}else{
+    obj["name"]='kacsa'
+}
+console.log(obj)
+console.log(obj["name"])
+console.log(obj.age)
