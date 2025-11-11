aa97a1d4498c073b1a8d38587abb2dbd61f5fb73
krisztian <szalkai.krisztian_2022c@bolyai-szakkozep.hu>
2025-11-11 20:35:10 +0100

    utolso allapot

--- table_row/kitero.js
+++ table_row/kitero.js
@@ -39,10 +39,10 @@ const arr =[
         mu:'A fizikusok',
     }      
 ]
-
+//table letrahozasa
 const table = document.createElement('table')
 document.body.appendChild(table)
-
+//thead
 const thead = document.createElement('thead')
 table.appendChild(thead)
 
@@ -58,129 +58,123 @@ for(const husi of tomb){
 
 const tbody = document.createElement('tbody')
 table.appendChild(tbody)
+tbody.id='teremtett'
 
-for(const k of arr){
-    
-    const trd = document.createElement('tr')
-    tbody.appendChild(trd)  
-    //NEMZET
-    const tdN = document.createElement('td')
-    tdN.innerText=k.nemzet
-    trd.appendChild(tdN)
+renderTableBody(arr)
 
-    tdN.addEventListener('click',function (e){//interakcio ha kattintasz megjelöli a tdN-t .marked-el
-        /**@type {HTMLTableCellElement} */
-        const target=e.target//egyik eleme
-        target.classList.add('marked')//.marked css
+/**
+ * @param {{nemzet:string,szerzo:string,mu:string,szerzo2?:string,mu2?:string}[]} gyujtemeny
+*/
+function renderTableBody(gyujtemeny){
+    const tbodykepviselo = document.getElementById('teremtett')
+    tbodykepviselo.innerHTML=''
 
-    })
+    for(const k of gyujtemeny){
 
-    //SZERZO
-    const tdSz = document.createElement('td')
-    tdSz.innerText=k.szerzo
-    trd.appendChild(tdSz)
-    //MŰ
-    const tdM = document.createElement('td')
-    tdM.innerText=k.mu
-    trd.appendChild(tdM)
-    //?
-    if(k.szerzo2 !== undefined && k.mu2 !== undefined){
-        tdN.rowSpan=2
+        const trd = document.createElement('tr')
+        tbodykepviselo.appendChild(trd)  
+        //NEMZET
+        const tdN = document.createElement('td')
+        tdN.innerText=k.nemzet
+        trd.appendChild(tdN)
 
-        const tr = document.createElement('tr')
-        tbody.appendChild(tr)
+        tdN.addEventListener('click',function (e){//interakcio ha kattintasz megjelöli a tdN-t .marked-el
+            /**@type {HTMLTableCellElement} */
+            const target=e.target//egyik eleme
+            target.classList.add('marked')//.marked css
 
-        //SZERZO 2
-        const tdSz2 = document.createElement('td')
-        tdSz2.innerText=k.szerzo2
-        tr.appendChild(tdSz2)
-        //MŰ 2
-        const tdM2 = document.createElement('td')
-        tdM2.innerText=k.mu2
-        tr.appendChild(tdM2)
+        })
+
+        //SZERZO
+        const tdSz = document.createElement('td')
+        tdSz.innerText=k.szerzo
+        trd.appendChild(tdSz)
+        //MŰ
+        const tdM = document.createElement('td')
+        tdM.innerText=k.mu
+        trd.appendChild(tdM)
+        //? van-e
+        if(k.szerzo2 !== undefined && k.mu2 !== undefined){
+            tdN.rowSpan=2
+
+            const tr = document.createElement('tr')
+            tbodykepviselo.appendChild(tr)
+
+            //SZERZO 2
+            const tdSz2 = document.createElement('td')
+            tdSz2.innerText=k.szerzo2
+            tr.appendChild(tdSz2)
+            //MŰ 2
+            const tdM2 = document.createElement('td')
+            tdM2.innerText=k.mu2
+            tr.appendChild(tdM2)
+        }
     }
 }
-//hf kommentek
+//hf kommentek, 3 ,14 ,15
+
+
+/**@type {{nemzet:string,szerzo:string,mu:string,szerzo2?:string,mu2?:string}} */
+const arr2 =[]
+
 /**@type {HTMLFormElement} */
 const htmlform = document.getElementById('htmlform')//form lrtrehoz
+
 addEventListener('submit',function (e){//ha submitolják a gombal akkor hzzáadja a táblához
     e.preventDefault()//nem kell az eredeti
-    /**@type {HTMLFormElement} */
-    const target =e.target //lerovidites
-
-    /**@type {HTMLInputElement} */
-    const nemzetisegelem= target.querySelector('#nemzetiseg')//a lista cimkéire utal
-    /**@type {string} */
-    const neS=nemzetisegelem.value//string lesz
-
-    /**@type {HTMLInputElement} */
-    const szerzoelemelso= target.querySelector('#szerzo1')//a lista cimkéire utal
-    /**@type {string} */
-    const szeS=szerzoelemelso.value//string lesz
-
-    /**@type {HTMLInputElement} */
-    const muelem= target.querySelector('#mu1')//a lista cimkéire utal
-    /**@type {string} */
-    const meS=muelem.value//string lesz
-
-    /**@type {HTMLInputElement} */
-    const szerzoelemmasodik= target.querySelector('#szerzo2')//a lista cimkéire utal
-    /**@type {string} */
-    const szemS=szerzoelemmasodik.value//string lesz
-
-    /**@type {HTMLInputElement} */
-    const muelemmasodik= target.querySelector('#mu2')//a lista cimkéire utal
-    /**@type {string} */
-    const memS=muelemmasodik.value//string lesz
 
     //a lista cimkei:
     /**@type {{nemzet:string,szerzo:string,mu:string,szerzo2?:string,mu2?:string}} */
     const obje ={}//ures de vannak cimkei
 
+    /**@type {HTMLFormElement} */
+    const target =e.target //lerovidites
+    
+    //----------------------------------------------------------------------------------
+    /**@type {HTMLInputElement} */
+    const nemzetisegelem= target.querySelector('#nemzetiseg')//a lista cimkéire utal
+    /**@type {string} */
+    const neS=nemzetisegelem.value//string lesz
     obje.nemzet = neS//egyes cimkekhez hozzarendelem a stringe alakitott targeteket
-    obje.szerzo= szeS
+    
+    //----------------------------------------------------------------------------------
+    /**@type {HTMLInputElement} */
+    const szerzoelemelso= target.querySelector('#szerzo1')//a lista cimkéire utal
+    /**@type {string} */
+    const szeS=szerzoelemelso.value//string lesz
+    obje.szerzo= szeS //HOZZAADAOM AZ OBJHEZ
+    
+    //----------------------------------------------------------------------------------
+    /**@type {HTMLInputElement} */
+    const muelem= target.querySelector('#mu1')//a lista cimkéire utal
+    /**@type {string} */
+    const meS=muelem.value//string lesz
     obje.mu =meS
+    
+    //----------------------------------------------------------------------------------
+    /**@type {HTMLInputElement} */
+    const szerzoelemmasodik= target.querySelector('#szerzo2')//a lista cimkéire utal
+    /**@type {string} */
+    const szemS=szerzoelemmasodik.value//string lesz
     obje.szerzo2=szemS
+    
+    //----------------------------------------------------------------------------------
+    /**@type {HTMLInputElement} */
+    const muelemmasodik= target.querySelector('#mu2')//a lista cimkéire utal
+    /**@type {string} */
+    const memS=muelemmasodik.value//string lesz
     obje.mu2 = memS
-
-    const asd = document.getElementById('alap')
-    const trd = document.createElement('tr')
-    asd.appendChild(trd)  
-    //NEMZET
-    const tdN = document.createElement('td')
-    tdN.innerText=obje.nemzet
-    trd.appendChild(tdN)
-
-    tdN.addEventListener('click',function (e){
-        /**@type {HTMLTableCellElement} */
-        const target=e.target
+    
+    //----------------------------------------------------------------------------------
+    const alap = document.getElementById('alap')//html tabla idje
+    const freddy = alap.querySelector('.marked')
+    if(freddy !==null){
+        alap.classList.remove('marked')
+    }else{
         target.classList.add('marked')
-
-    })
-
-    //SZERZO
-    const tdSz = document.createElement('td')
-    tdSz.innerText=obje.szerzo
-    trd.appendChild(tdSz)
-    //MŰ
-    const tdM = document.createElement('td')
-    tdM.innerText=obje.mu
-    trd.appendChild(tdM)
-    //?
-    if(obje.szerzo2 && obje.mu2){
-        tdN.rowSpan=2
-
-        const tr = document.createElement('tr')
-        asd.appendChild(tr)
-
-        //SZERZO 2
-        const tdSz2 = document.createElement('td')
-        tdSz2.innerText=obje.szerzo2
-        tr.appendChild(tdSz2)
-        //MŰ 2
-        const tdM2 = document.createElement('td')
-        tdM2.innerText=obje.mu2
-        tr.appendChild(tdM2)
     }
 
+    arr2.push(obje)
+    renderTableBody(arr2)
 })
+renderTableBody(arr2)
